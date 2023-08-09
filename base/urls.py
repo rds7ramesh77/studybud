01ad7e99f0c491c090e9a4a841a3ff7c8d9dd218
rds7ramesh77 <rameshsapkota416@gmail.com>
2023-08-09 20:09:56 +0545

    login logout

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+  
+
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/',views.createRoom,name='create-room'),
--- base/views.py
+++ base/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render,redirect
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from .models import Room,Topic
 from .forms import RoomForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+
 # rooms=[
 #     {'id':1,'name':'Lets learn python','topic':'Python','description':'This is a python study room'},
 #     {'id':2,'name':'Lets learn django','topic':'Django','description':'This is a django study room'},
@@ -19,6 +24,31 @@ from .forms import RoomForm
 
 # ]
 
+def loginPage(request):
+    if request.method=='POST':
+     username=request.POST.get('username')
+     password=request.POST.get('password')
+
+     try:
+         user=User.objects.get(username=username)
+        
+     except:
+            messages.error(request,'Username does not exist')
+        
+        
+     user=authenticate(request,username=username,password=password)
+     if user is not None:
+            login(request,user)
+            return redirect('home')
+     else:
+            messages.error(request,'Username or password is incorrect')
+
+    context={}
+    return render(request, 'base/login_register.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
