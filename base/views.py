77e5fd5cb940322acb4c2370ebf0790efd946558
rds7ramesh77 <rameshsapkota416@gmail.com>
2023-08-20 19:46:18 +0545

    user activity

--- base/views.py
+++ base/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render,redirect
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from .models import Room,Topic,Message
-from .forms import RoomForm
+from .forms import RoomForm,UserForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
 from django.contrib.auth import authenticate,login,logout
-from django.http import HttpResponse
+from django.http import HttpResponse 
 
 # rooms=[
 #     {'id':1,'name':'Lets learn python','topic':'Python','description':'This is a python study room'},
@@ -198,3 +198,16 @@ def deleteMessage(request,pk):
         message.delete()
         return redirect('home')
     return render(request,"base/delete.html",{'obj':message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user=request.user
+    form=UserForm(instance=user)
+
+    if request.method=='POST':
+        form=UserForm(request.POST,instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile',pk=user.id)
+
+    return render(request,"base/update-user.html" ,{'form':form})
