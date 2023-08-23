c5501368f8a7f7152b7df28a20a9b822a72e76b4
rds7ramesh77 <rameshsapkota416@gmail.com>
2023-08-23 21:23:33 +0545

    rest api final

--- base/api/urls.py
+++ base/api/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('rooms/', views.getRooms, name="rooms"),
+    path('rooms/<str:pk>/', views.getRoom, name="room"),
 ]
